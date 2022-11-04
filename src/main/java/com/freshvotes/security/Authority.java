1c4b5cc1b9d02bbb5aa5d912918c2272aaec94ba
Jason Yao <jasonyao.jy@gmail.com>
2022-11-03 23:17:46 -0400

    Implemented User Details

---
+++ src/main/java/com/freshvotes/security/Authority.java
@@ -0,0 +1,42 @@
+package com.freshvotes.security;
+
+import com.freshvotes.domain.User;
+import org.springframework.security.core.GrantedAuthority;
+
+import javax.persistence.*;
+
+@Entity
+public class Authority implements GrantedAuthority {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @ManyToOne
+    private User user;
+
+    private String authority;
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public String getAuthority() {
+        return this.authority;
+    }
+
+    public void setAuthority(String authority) {
+        this.authority = authority;
+    }
+}
