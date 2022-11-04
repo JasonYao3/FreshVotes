0987397e52fa0c8752290f7559fedf5908269914
Jason Yao <jasonyao.jy@gmail.com>
2022-11-03 21:47:22 -0400

    Adding Password Encoder

--- src/main/java/com/freshvotes/security/WebSecurityConfiguration.java
+++ src/main/java/com/freshvotes/security/WebSecurityConfiguration.java
@@ -13,15 +13,22 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 public class WebSecurityConfiguration   {
 
-    private static final String ENCODED_PASSWORD = "password";
     @Bean
-    public InMemoryUserDetailsManager userDetailsService() {
-        UserDetails user = User.builder()
-                .username("user")
-                .password("password")
-                .roles("USER")
-                .build();
-        return new InMemoryUserDetailsManager(user);
+    public PasswordEncoder getPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Configuration
+    public class SecurityConfiguration {
+        @Bean
+        public InMemoryUserDetailsManager userDetailsService() {
+            UserDetails user = User.builder()
+                    .username("user")
+                    .password(getPasswordEncoder().encode("password"))
+                    .roles("USER")
+                    .build();
+            return new InMemoryUserDetailsManager(user);
+        }
     }
 
     @Bean
@@ -29,15 +36,9 @@ public class WebSecurityConfiguration   {
         http.csrf().disable()
                 .authorizeRequests()
                 .antMatchers("/").permitAll()
-                .antMatchers("/login").permitAll()
                 .anyRequest().hasRole("USER").and()
                 .formLogin().loginPage("/login").permitAll().and()
                 .logout().logoutUrl("/logout").permitAll();
         return http.build();
-    }
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
     }
 }
