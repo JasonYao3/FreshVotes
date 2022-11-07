bd51fa8bd53e855cb18a23259334c89b46acffda
Jason Yao <jasonyao.jy@gmail.com>
2022-11-07 12:53:04 -0500

    Creating a feature request

---
+++ src/main/java/com/freshvotes/service/FeatureService.java
@@ -0,0 +1,38 @@
+package com.freshvotes.service;
+
+import com.freshvotes.domain.Feature;
+import com.freshvotes.domain.Product;
+import com.freshvotes.repositories.FeatureRepository;
+import com.freshvotes.repositories.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class FeatureService {
+
+    @Autowired
+    private FeatureRepository featureRepo;
+
+    @Autowired
+    private ProductRepository productRepo;
+
+    public Feature createFeature(Long productId) {
+        Feature feature = new Feature();
+
+        Optional<Product> productOpt = productRepo.findById(productId);
+
+        if (productOpt.isPresent()) {
+            Product product = productOpt.get();
+
+            feature.setProduct(product);
+            product.getFeatures().add(feature);
+
+            feature.setStatus("Pending review");
+
+            return featureRepo.save(feature);
+        }
+        return feature;
+    }
+}
