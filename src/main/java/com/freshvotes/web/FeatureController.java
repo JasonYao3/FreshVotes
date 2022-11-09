9e90c68e804cf7c25addd2974e34981a92ae57a9
Jason Yao <jasonyao.jy@gmail.com>
2022-11-08 21:23:51 -0500

    Making better workflow and UX

--- src/main/java/com/freshvotes/web/FeatureController.java
+++ src/main/java/com/freshvotes/web/FeatureController.java
@@ -2,17 +2,25 @@ package com.freshvotes.web;
 
 import com.freshvotes.domain.Feature;
 import com.freshvotes.service.FeatureService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
 import java.util.Optional;
 
 @Controller
 @RequestMapping("/products/{productId}/features")
 public class FeatureController {
 
+    Logger log = LoggerFactory.getLogger(FeatureController.class);
     @Autowired
     private FeatureService featureService;
 
@@ -20,7 +28,7 @@ public class FeatureController {
     public String createFeature(@PathVariable Long productId) {
         Feature feature = featureService.createFeature(productId);
 
-        return "redirect:/products/"+productId+"/features/"+feature.getId();
+        return "redirect:/products/" + productId + "/features/" + feature.getId();
 
     }
 
@@ -40,8 +48,15 @@ public class FeatureController {
     @PostMapping("{featureId}")
     public String updateFeature(Feature feature, @PathVariable Long productId, @PathVariable Long featureId) {
         feature = featureService.save(feature);
+        String encodedProductName;
 
-        return "redirect:/products/"+productId+"/features/"+feature.getId();
+        try {
+            encodedProductName = URLEncoder.encode(feature.getProduct().getName(), "UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            log.warn("Unable to encode the URL string: " + feature.getProduct().getName() + ", redirecting to dashboard instead of the intended product user view page.");
+            return "redirect:/dashboard";
+        }
+
+        return "redirect:/p/" + encodedProductName;
     }
-
 }
