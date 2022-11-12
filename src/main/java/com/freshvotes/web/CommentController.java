ea2f64d3693a8a2f8de542ce8b8015de11997f7a
Jason Yao <jasonyao.jy@gmail.com>
2022-11-12 15:54:12 -0500

    Finishing comments functionality

--- src/main/java/com/freshvotes/web/CommentController.java
+++ src/main/java/com/freshvotes/web/CommentController.java
@@ -1,28 +1,65 @@
 package com.freshvotes.web;
 
 import com.freshvotes.domain.Comment;
+import com.freshvotes.domain.Feature;
+import com.freshvotes.domain.User;
 import com.freshvotes.repositories.CommentRepository;
+import com.freshvotes.repositories.FeatureRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.Date;
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/products/{productId}/features/{featureId}/comments")
 public class CommentController {
-
     @Autowired
     public CommentRepository commentRepo;
+    @Autowired
+    public FeatureRepository featureRepo;
 
     @GetMapping("")
     @ResponseBody
-    public List<Comment> getComments(@PathVariable Long featureId) {
-        List<Comment> findByFeatureId = commentRepo.findByFeatureId(featureId);
+    public List<Comment> getComments (@PathVariable Long featureId) {
 
+        List<Comment> findByFeatureId = commentRepo.findByFeatureId(featureId);
+        System.out.println(findByFeatureId);
         return findByFeatureId;
+    }
+
+    @PostMapping("")
+    public String postComment(@AuthenticationPrincipal User user, @PathVariable Long productId,
+                              @PathVariable Long featureId, Comment rootComment, @RequestParam(required=false) Long parentId,
+                              @RequestParam(required=false) String childCommentText) {
+        Optional<Feature> featureOpt = featureRepo.findById(featureId);
+        // save a root level comment here
+        if (!StringUtils.isEmpty(rootComment.getText())) {
+            populateCommentMetadata(user, featureOpt, rootComment);
+            commentRepo.save(rootComment);
+        }
+        // save a child level comment here
+        else if (parentId != null) {
+            Comment comment = new Comment();
+            Optional<Comment> parentCommentOpt = commentRepo.findById(parentId);
+            if (parentCommentOpt.isPresent())
+                comment.setComment(parentCommentOpt.get());
+            comment.setText(childCommentText);
+            populateCommentMetadata(user, featureOpt, comment);
+            commentRepo.save(comment);
+        }
+
+        return "redirect:/products/" + productId + "/features/" + featureId;
+    }
+
+    private void populateCommentMetadata(User user, Optional<Feature> featureOpt, Comment comment) {
+        if (featureOpt.isPresent())
+            comment.setFeature(featureOpt.get());
+        comment.setUser(user);
+        comment.setCreatedDate(new Date());
     }
 }
